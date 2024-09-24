0de01385a419c7587548bcbec72fd127a2d3d1b7
Arhizmei <arhizmei@gmail.com>
2024-09-24 16:21:24 +0300

    add recycleView and create cardView

---
+++ app/src/main/java/com/kravchenkovadim/part10_89_androidpro/RecyclerViewItem.java
@@ -0,0 +1,11 @@
+package com.kravchenkovadim.part10_89_androidpro;
+
+public class RecyclerViewItem {
+
+
+    private int imageResource;
+    private String text1;
+    private String text2;
+    
+
+}
