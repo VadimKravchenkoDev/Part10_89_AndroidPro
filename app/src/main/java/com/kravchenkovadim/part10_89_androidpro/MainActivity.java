70512ddb72dc49450cbe1961d54129f179e77800
Arhizmei <arhizmei@gmail.com>
2024-09-24 16:30:56 +0300

    save1

--- app/src/main/java/com/kravchenkovadim/part10_89_androidpro/MainActivity.java
+++ app/src/main/java/com/kravchenkovadim/part10_89_androidpro/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayList<RecyclerViewItem> recyclerViewItems = new ArrayList<>();
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_anchor_24,
+                "Anchor", "Yahu!"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_downhill_skiing_24,
+                "Downhill_skiing", "Yahu!"));
+        recyclerViewItems.add(new RecyclerViewItem(R.drawable.baseline_emoji_objects_24,
+                "Lamp", "Yahu!"));
     }
 }
